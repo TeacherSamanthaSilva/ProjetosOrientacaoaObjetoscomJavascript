5b08718d6568178477c1c713c6ce598fdd2ec3e0
TeacherSamanthaSilva <75630998+TeacherSamanthaSilva@users.noreply.github.com>
2025-05-19 20:59:26 -0300

    exerc
    Nesse commit fiz o exercicio 4

--- Exercicio 3/script.js
+++ Exercicio 3/script.js
@@ -1,12 +1,23 @@
-function Ninja(nome){
+function Ninja(nome,qtd){
     this.nome = nome;
+    this.estoqueShuriken = qtd;
     this.atirarShuriken = function(){
-        console.log("O ninja atirou a Shuriken");
+        if(this.estoqueShuriken > 0 ){
+            console.log("O ninja atirou a shuriken");
+            this.estoqueShuriken -=1;
+        }else{
+
+            console.log("O ninja não pode mais atirar Shurikens");
+
+        }
+        
     }
 }
 
-let naruto = new Ninja('Naruto');
+let naruto = new Ninja('Naruto',3);
 
 console.log(naruto);
 
+naruto.atirarShuriken();
+naruto.atirarShuriken();
 naruto.atirarShuriken();
