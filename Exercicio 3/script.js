01f39487d13f689835436fa0251b3d99cbd72f5a
TeacherSamanthaSilva <75630998+TeacherSamanthaSilva@users.noreply.github.com>
2025-05-22 22:21:36 -0300

    fim exercicio5
    nesse commit finalizei o exercicio 5

--- Exercicio 3/script.js
+++ Exercicio 3/script.js
@@ -1,10 +1,11 @@
 function Ninja(nome,qtd){
     this.nome = nome;
     this.estoqueShuriken = qtd;
-    this.atirarShuriken = function(){
+    this.atirarShuriken = function(inimigo){
         if(this.estoqueShuriken > 0 ){
             console.log("O ninja atirou a shuriken");
             this.estoqueShuriken -=1;
+            Object.vivo = false;
         }else{
 
             console.log("O ninja não pode mais atirar Shurikens");
@@ -14,10 +15,17 @@ function Ninja(nome,qtd){
     }
 }
 
+function inimigo(){
+    this.nome = this.nome;
+    this.vivo = true;
+}
+
+let orochimaru = new inimigo('Orochimaru');
+
 let naruto = new Ninja('Naruto',3);
 
 console.log(naruto);
 
-naruto.atirarShuriken();
-naruto.atirarShuriken();
-naruto.atirarShuriken();
+console.log(orochimaru);
+
+naruto.atirarShuriken(orochimaru);
