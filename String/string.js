53b0a96dbd7665a6a64a889647df160ea1abdb03
TeacherSamanthaSilva <75630998+TeacherSamanthaSilva@users.noreply.github.com>
2025-06-11 20:10:25 -0300

    string
    nesse commit eu estudei o que é string

---
+++ String/string.js
@@ -0,0 +1,11 @@
+let s1 = new String('teste');
+let s2 = 'testando'; 
+
+console.log(s1);
+console.log(s2);
+
+console.log(s1.length);
+console.log(s2.length);// mostra o tamanho de caracteres
+
+console.log(s1.toLowerCase);// coloca tudo em minúscula
+console.log(s2.toUpperCase);// coloca tudo em maiúscula
