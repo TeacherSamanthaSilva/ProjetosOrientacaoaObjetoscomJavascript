b039db01ed938babca30de439aa747432abb2e12
TeacherSamanthaSilva <75630998+TeacherSamanthaSilva@users.noreply.github.com>
2025-06-12 22:05:39 -0300

    Update script.js
    nesse  commit estudei o que é math

--- Math/script.js
+++ Math/script.js
@@ -1,4 +1,4 @@
 console.log(Math.PI);// mostra o valor de PI
 console.log(Math.random(10)); // mostra um número aleatórios
-console.log(Math.pow(12,2));
-console.log(Math.sqrt(9));
+console.log(Math.pow(12,2));// calculo de potenciação
+console.log(Math.sqrt(9));// cálculo de radiciação
